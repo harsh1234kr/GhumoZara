dde4ba9e72795beba224a318ea5d728f1900dd87
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-10-02 23:45:54 +0530

    Create main_screen.swift

---
+++ main_screen.swift
@@ -0,0 +1,60 @@
+struct MainView: View {
+    var body: some View {
+        TabView {
+            CategoryView(category: "Attractions", imageName: "star.fill")
+                .tabItem {
+                    Label("Attractions", systemImage: "star.fill")
+                }
+            
+            CategoryView(category: "Restaurants", imageName: "fork.knife")
+                .tabItem {
+                    Label("Restaurants", systemImage: "fork.knife")
+                }
+            
+            CategoryView(category: "Accommodations", imageName: "house.fill")
+                .tabItem {
+                    Label("Stay", systemImage: "house.fill")
+                }
+        }
+    }
+}
+
+struct CategoryView: View {
+    let category: String
+    let imageName: String
+    @State private var places: [Place] = []
+    
+    var body: some View {
+        NavigationView {
+            List(places) { place in
+                NavigationLink(destination: DetailView(place: place)) {
+                    HStack {
+                        Image(systemName: imageName)
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        VStack(alignment: .leading) {
+                            Text(place.name).font(.headline)
+                            Text(place.description).font(.subheadline)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(category)
+        }
+    }
+}
+
+// Placeholder DetailView
+struct DetailView: View {
+    let place: Place
+
+    var body: some View {
+        Text(place.name)
+    }
+}
+
+struct Place: Identifiable {
+    var id: Int
+    var name: String
+    var description: String
+}
