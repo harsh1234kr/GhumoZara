294ef2e5e62c1bd3732380d497d275ee00106da3
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-10-03 23:05:52 +0530

    Update detail_view.swift

--- detail_view.swift
+++ detail_view.swift
@@ -7,6 +7,6 @@ struct DetailView: View {
         center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868), // Default coordinates
         span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
     )
-
-
- 
+        var body: some View {
+        ScrollView {
+          
