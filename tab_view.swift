42d90333cd0f5fdb8c10402c067107497120f4c9
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-10-05 21:43:16 +0530

    Update tab_view.swift

--- tab_view.swift
+++ tab_view.swift
@@ -1,28 +1,6 @@
 import SwiftUI
 
-struct MainTabView: View {
-    var body: some View {
-        TabView {
-            AttractionsView()
-                .tabItem {
-                    Image(systemName: "star.fill")
-                    Text("Attractions")
-                }
-            
-            RestaurantsView()
-                .tabItem {
-                    Image(systemName: "fork.knife")
-                    Text("Restaurants")
-                }
-            
-            AccommodationsView()
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Stays")
-                }
-        }
-    }
-}
+
 struct AttractionsView: View {
     // Attraction view code here
     var body: some View {
