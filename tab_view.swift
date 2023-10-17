c7a6f26f2b7a04ba5f5e24c0b7b6792db4a94478
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-10-17 22:54:04 +0530

    Update tab_view.swift

--- tab_view.swift
+++ tab_view.swift
@@ -25,12 +25,7 @@ struct MainTabView: View {
             
 
 
-struct AttractionsView: View {
-    // Attraction view code here
-    var body: some View {
-        Text("List of attractions will appear here.")
-    }
-}
+
 struct RestaurantsView: View {
     // Restaurants view code here
     var body: some View {
