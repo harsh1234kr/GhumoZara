1c89f97d17714e2e085ef343dd82e7e2d678bc31
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-10-25 20:45:22 +0530

    Update tab_view.swift

--- tab_view.swift
+++ tab_view.swift
@@ -39,13 +39,6 @@ struct AccommodationsView: View {
         Text("List of accommodations will appear here.")
     }
 }
-struct MainTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainTabView()
-    }
-}
-
-
 
 
 
