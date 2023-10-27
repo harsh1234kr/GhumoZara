b18d2c257bb518fa0de07939e0a4159d4ebfcf43
Harsh Kumar <85002498+harsh1234kr@users.noreply.github.com>
2023-10-27 10:23:48 +0530

    Update category_view.swift

--- category_view.swift
+++ category_view.swift
@@ -1,5 +1,29 @@
 import SwiftUI
 
+struct CategoryView: View {
+    var categoryTitle: String
+    @State private var searchText: String = ""
+    @State private var places: [Place] = samplePlaces // Placeholder data
+    
+    var body: some View {
+        VStack {
+            SearchBar(text: $searchText, placeholder: "Search \(categoryTitle)")
+            
+            List {
+                ForEach(places.filter {
+                    searchText.isEmpty ? true : $0.name.contains(searchText)
+                }) { place in
+                    NavigationLink(destination: DetailView(place: place)) {
+                        PlaceRow(place: place)
+                    }
+                }
+            }
+            .navigationTitle(categoryTitle)
+        }
+    }
+}
+
+
 struct SearchBar: View {
     @Binding var text: String
     var placeholder: String var body: some View {
